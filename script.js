370baebc9eeb59a7e85b2fa3c9c922fa83cd8a83
JasonWilger <jkw500@gmail.com>
2020-08-21 15:59:22 -0400

    added recipe JS

---
+++ script.js
@@ -0,0 +1,14 @@
+var numberTwo = 2;
+var oneFourth = "1/4";
+var cookName = "Jason";
+
+function marinaraSauce (somename) {
+    console.log("Hey, " + cookName + "this is my marinara sauce recipe!");
+    console.log( "Heat a medium-large saucepan over medium heat.");
+    console.log(" Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes");
+    console.log("Add " + oneFourth + "cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log("stir in" + oneFourth + "cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+marinaraSauce(cookName)
